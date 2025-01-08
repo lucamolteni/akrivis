63f80b6362f0becbb9d59309f021dbd1323775cd
Luca Molteni <volothamp@gmail.com>
2025-01-08 11:01:24 +0100

    Fix start-in-tmux after database -> script rename

--- start-in-tmux-tabs.sh
+++ start-in-tmux-tabs.sh
@@ -2,7 +2,7 @@
 
 tmux new-session -d -s scorecard -n "Processor" 'cd akrivis-processor; mvn compile quarkus:dev'
 
-tmux new-window -t scorecard -n "Database" 'cd database; docker-compose -f compose.yaml up'
+tmux new-window -t scorecard -n "Database" 'cd scripts; podman compose -f compose.yaml up'
 tmux new-window -t scorecard -n "RestMock" 'cd examples/rest-service-mock; mvn compile quarkus:dev -Dquarkus.http.port=8081 -Ddebug=5006'
 tmux new-window -t scorecard -n "Evaluator" 'cd akrivis-evaluator; mvn compile quarkus:dev -Dquarkus.http.port=8082 -Ddebug=5007'
 tmux new-window -t scorecard -n "Ingestor" 'cd akrivis-ingestor; mvn compile quarkus:dev -Dquarkus.http.port=8083 -Ddebug=5008'
